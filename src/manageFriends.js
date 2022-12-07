05d8edb3200ad4862399f32115369f1e56aed68d
Dennis Njogu <dennis.njogu@student.moringaschool.com>
2022-12-08 00:22:48 +0300

    passed all

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,17 @@
-export function manageFriends(state, action) {
+const initialState = {
+  friends: []
+}
+
+export function manageFriends(state = initialState, action) {
   // your code here
+  const {payload} = action
+  const {friends} = state
+  switch(action.type){
+    case "friends/add":      
+      return { friends: [ ...friends, payload]}
+    case "friends/remove":
+      return {friends: state.friends.filter(({id}) => id !== payload)}
+    default:
+      return state
+  }
 }
